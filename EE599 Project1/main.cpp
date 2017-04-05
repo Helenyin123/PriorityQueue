f99a870a95b75ff6757d9ab883f8b865e3aa1222
Chang Yin <changyin@Changs-MacBook-Pro.local>
2017-04-05 16:36:01 -0700

    Initial Commit

---
+++ EE599 Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EE599 Project1
+//
+//  Created by Chang Yin on 4/5/17.
+//  Copyright © 2017 Chang Yin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
